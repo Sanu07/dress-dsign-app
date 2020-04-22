62b4ac45cb8126b04fc8ff42362dd4877cca1df9
Sanu Ghosh <whyalwaysme07@gmail.com>
2020-04-22 23:27:10 +0530

    add entity and lombok details

--- dress-design-backend/src/main/java/com/fashion/model/Dress.java
+++ dress-design-backend/src/main/java/com/fashion/model/Dress.java
@@ -1,6 +1,26 @@
 package com.fashion.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+@Entity
+@Table(name = "DRESS_DETAILS")
 public class Dress {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "DRESS_ID")
 	private long id;
 	private String dressId;
 	private DressMeasurements dressMeasurements;
--- dress-design-backend/src/main/java/com/fashion/model/Orders.java
+++ dress-design-backend/src/main/java/com/fashion/model/Orders.java
@@ -3,12 +3,24 @@ package com.fashion.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor
+@Entity
+@Table(name = "ORDER_DETAILS")
 public class Orders {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ORDER_ID")
