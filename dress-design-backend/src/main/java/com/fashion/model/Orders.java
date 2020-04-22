c06931fa562873120ed35012d9ad4b33401b11d9
Sanu Ghosh <whyalwaysme07@gmail.com>
2020-04-22 23:26:11 +0530

    add entity and lombok annotations

--- dress-design-backend/src/main/java/com/fashion/model/Orders.java
+++ dress-design-backend/src/main/java/com/fashion/model/Orders.java
@@ -2,8 +2,16 @@ package com.fashion.model;
 
 import java.util.Date;
 
-public class Orders {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Orders {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ORDER_ID")
 	private long id;
 	private String orderNo;
 	private Dress dress;
