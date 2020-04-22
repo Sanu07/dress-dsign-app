7149295fcc9ca9cf1f12e122dc6c24d0ceb0faf2
Sanu Ghosh <whyalwaysme07@gmail.com>
2020-04-22 23:04:13 +0530

    add dress and orders model class

---
+++ dress-design-backend/src/main/java/com/fashion/model/Dress.java
@@ -0,0 +1,10 @@
+package com.fashion.model;
+
+public class Dress {
+	private long id;
+	private String dressId;
+	private DressMeasurements dressMeasurements;
+	private String dressType;
+	private Orders orders;
+	private Customer customer;
+}
--- dress-design-backend/src/main/java/com/fashion/model/Orders.java
+++ dress-design-backend/src/main/java/com/fashion/model/Orders.java
@@ -1,11 +1,15 @@
 package com.fashion.model;
 
+import java.util.Date;
 
 public class Orders {
 
 	private long id;
-	
-	// 
 	private String orderNo;
-	
+	private Dress dress;
+	private Date orderReceived;
+	private Date estimatedDeliveryDate;
+	private Date orderDelivered;
+	private Payment payment;
+	private Customer customer;
 }
