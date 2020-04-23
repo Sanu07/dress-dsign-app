d8b166e20b862003b6ef56045053ac908851c807
Sanu Ghosh <whyalwaysme07@gmail.com>
2020-04-24 01:13:51 +0530

    changed to lowercase

--- dress-design-ui/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ dress-design-ui/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -8,7 +8,7 @@ import { IconsComponent } from '../../components/icons/icons.component';
 import { MapsComponent } from '../../components/maps/maps.component';
 import { NotificationsComponent } from '../../components/notifications/notifications.component';
 import { UpgradeComponent } from '../../components/upgrade/upgrade.component';
-import { OrdersComponent } from 'app/components/Orders/orders.component';
+import { OrdersComponent } from 'app/components/orders/orders.component';
 import { PaymentsComponent } from 'app/components/payments/payments.component';
 import { CustomersComponent } from 'app/components/customers/customers.component';
 import { MeasurementsComponent } from 'app/components/measurements/measurements.component';
