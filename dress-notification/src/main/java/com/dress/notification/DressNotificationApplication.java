b169d46a1ff9762c501e8b85af45b2fc1fc17ea9
Sanu07 <whyalwaysme07@gmail.com>
2021-05-05 14:30:12 +0530

    Initial maven structure for notification service
    Barebone implementation for Notification service

---
+++ dress-notification/src/main/java/com/dress/notification/DressNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.dress.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressNotificationApplication.class, args);
+	}
+
+}
---
+++ dress-notification/src/test/java/com/dress/notification/DressNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dress.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
